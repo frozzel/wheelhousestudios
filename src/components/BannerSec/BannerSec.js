71320042b95702d3f40b3b4b291cce0645298ec9
Dennis Hickox <frozzel@me.com>
2024-03-08 14:11:30 -0500

    updated to new logo with color

--- src/components/BannerSec/BannerSec.js
+++ src/components/BannerSec/BannerSec.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import VideoModal from '../VideoSection'
+import VideoModal from '../VideoSection2'
 const BannerSec = (props) => {
     return(
         <section className="wpo-banner-section">
-            <h4>We Are Waiting for Celebrate Your Love</h4>
+            <h4>We Are Waiting To Celebrate Your Love</h4>
             <VideoModal/>
         </section>
     )
