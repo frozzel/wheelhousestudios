c67773baf47e8d90b4ef08b223bab92ba86e00a7
Dennis Hickox <frozzel@me.com>
2024-04-22 12:30:07 -0400

    minor fix

--- src/components/Testimonial/Testimonial.js
+++ src/components/Testimonial/Testimonial.js
@@ -14,6 +14,7 @@ const testimonial = [
         Des: "Pricing for a videographer wasn’t really in our budget until someone mentioned Quinton. He’s very personable and he made us feel very comfortable throughout the whole process! His prices are very reasonable & the quality of the video is amazing. We got our video back in a good amount of time and we LOVED our video, we get a ton of compliments on it. We would recommend him to anybody! He was awesome!",
         Title: 'Marissa Cox',
         Date: "23.05.2023",
+        event: "Wedding"
     },
     {
         id: '01',
@@ -21,13 +22,15 @@ const testimonial = [
         Des: "I am a very reserved person and I don’t like my picture being taken at all. However, Quinton’s personality put me at ease and I loved that he had so many different ideas! The wedding day was so stressful and hectic and I appreciate Quinton for making a schedule to make the photography portion as smooth as possible. Everyone from the wedding loved his photos. They are AMAZING. Everyone also loved how friendly and bubbly he was. To this day we still hear about how much people loved the photos or just loved him in general. So yes, we would 100% recommend Quinton to anyone.",
         Title: 'Veronica Cruz-Chambers',
         Date: "11.04.2023",
+        event: "Wedding"
     },
     {
         id: '01',
         tImg: ts3,
-        Des: "VI had never had to look for a photographer before, but I have heard horror stories about pricing and quality of photos. I knew Quinton would be an excellent choice simply because he has such an outstanding personality. He helped with where to take the photos, poses, and plenty of other things I didn't realize went into photography! Quinton turned some casual senior photos into something magical I can remember forever.",
+        Des: "I had never had to look for a photographer before, but I have heard horror stories about pricing and quality of photos. I knew Quinton would be an excellent choice simply because he has such an outstanding personality. He helped with where to take the photos, poses, and plenty of other things I didn't realize went into photography! Quinton turned some casual senior photos into something magical I can remember forever.",
         Title: 'Sofia Frenzel',
         Date: "10.27.2023",
+        event: "Senior Photos"
     }
 ]
 
@@ -71,7 +74,7 @@ const Testimonial = () => {
                                                     <p>{tesmnl.Des}</p>
                                                     <div className="wpo-testimonial-text-btm">
                                                         <h3>{tesmnl.Title}</h3>
-                                                        <span>Wedding- {tesmnl.Date}</span>
+                                                        <span>{tesmnl.event}- {tesmnl.Date}</span>
                                                     </div>
                                                 </div>
                                             </div>
